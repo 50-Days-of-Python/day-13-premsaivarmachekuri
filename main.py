fd2760a9a989d484451c86ab900dd1c4c8c4218e
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-01-26 22:29:07 +0530

    committed day 13 python challenge

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#Write your code here.
+import math
+def total_price(price,vat):
+    new_price = price + math.ceil((vat/100)*price)
+    return new_price
+num = list(map(int,input().split()))
+print(total_price(num[0],num[1]))
